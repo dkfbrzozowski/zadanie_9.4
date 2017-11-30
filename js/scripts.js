b221b4c57e0dabbc70295585330e303d3c5b73d9
Dawid Brzozowski <d.brzozowski@wp.pl>
2017-11-30 14:52:50 +0100

    Add trunk

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,18 @@
 
 function drawTree() {
-	for (i = 0; i < 5; i++) {
-		star = '';
-		for (j = i; j < 5; j++) {
-			star += ' ';
-		}
-		for (k = 0; k < (i * 2) - 1; k++) {
-			star += '*';
-		}
-		console.log(star);
-	}
+for (i = 0; i < 6; i++) {
+  star = '';
+  for (j = i; j < 6; j++) {
+    star += ' ';
+  }
+  for (k = 0; k < (i * 2) - 1; k++) {
+    star += '*';
+  }
+  console.log(star);
+}  
+for (i = 6; i < 9; i++) {
+    star = '    ***';
+  console.log(star);
+  }
 }
 drawTree();
